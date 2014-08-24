c76352a95b4fa122c5b1f7772e1a6d6188b443d0
Charles Hooper <charles@heroku.com>
2014-08-23 19:19:51 -0700

    move channels to their own file

--- bus.rb
+++ bus.rb
@@ -9,35 +9,8 @@
 # assumption #2: we'll just use redis lists for our channels
 
 require 'thread'
-require 'redis'
 require 'retries'
-
-class Channel
-  def initialize(redis_url, key, param={})
-    @url = redis_url
-    @key = key
-    @timeout = param[:timeout] || 10 # seconds
-  end
-
-  def connect
-    with_retries(:max_tries => 3) do
-      @redis.ping unless @redis.nil?
-      @redis = Redis.connect(url: @url) if @redis.nil? or not @redis.connected?
-      @redis
-    end
-  end
-
-  def pop
-    loop do
-      item = nil
-      with_retries(:max_tries => 3) do
-        self.connect
-        item = @redis.blpop(@key, @timeout)
-      end
-      return item unless item.nil?
-    end
-  end
-end
+require './channel.rb'
 
 class Bus
   def initialize(redis_urls, key, chan_params={})
---
+++ channel.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+require 'redis'
+require 'retries'
+
+class Channel
+  def initialize(redis_url, key, param={})
+    @url = redis_url
+    @key = key
+    @timeout = param[:timeout] || 10 # seconds
+  end
+
+  def connect
+    with_retries(:max_tries => 3) do
+      @redis.ping unless @redis.nil?
+      @redis = Redis.connect(url: @url) if @redis.nil? or not @redis.connected?
+      @redis
+    end
+  end
+
+  def pop
+    loop do
+      item = nil
+      with_retries(:max_tries => 3) do
+        self.connect
+        item = @redis.blpop(@key, @timeout)
+      end
+      return item unless item.nil?
+    end
+  end
+end
+
